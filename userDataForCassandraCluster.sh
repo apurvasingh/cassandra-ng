17d047068ed3eabc97d7762bef84ffd0d8624afb
Apurva Singh <asingh@Apurvas-MacBook-Pro.local>
2016-06-11 18:15:53 -0400

    Fixed minor issues with Tomcat config file creation

--- userDataForCassandraCluster.sh
+++ userDataForCassandraCluster.sh
@@ -42,14 +42,23 @@ echo "${INSTALLFOR}"
 
 if [ "${INSTALLFOR}" = "Ubuntu" -o "${INSTALLFOR}" = "Debian" ] ; then
 
+	apt-get -y update > /dev/null
 	apt-get -y install git > /dev/null
 	apt-get -y install wget > /dev/null
 
+#install Java in silent mode
+cat - <<-EOF >> /etc/apt/sources.list.d/webupd8team-java.list
+	# webupd8team repository list 
+	deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main
+EOF
 
-	#setup the Java8 repo
-	apt-get -y install python-software-properties > /dev/null
-	add-apt-repository ppa:webupd8team/java
+	apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xEEA14886
+
+	echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+	echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-set-selections
+
 	apt-get -y update > /dev/null
+	apt-get -y install oracle-java8-installer > /dev/null
 
 	#setup the Cassandra repo
 	echo "deb http://www.apache.org/dist/cassandra/debian 21x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
@@ -82,41 +91,41 @@ if [ "${INSTALLFOR}" = "Ubuntu" -o "${INSTALLFOR}" = "Debian" ] ; then
 
 	chown -R tomcat work/ temp/ logs/
 	
-cat << TOMCATCONFIG > /etc/init/tomcat.conf
+cat << 'TOMCATCONFIG' > /etc/init/tomcat.conf
+description "Tomcat Server"
 
-        description "Tomcat Server"
+  start on runlevel [2345]
+  stop on runlevel [!2345]
+  respawn
+  respawn limit 10 5
 
-         start on runlevel [2345]
-         stop on runlevel [!2345]
-         respawn
-         respawn limit 10 5
+  setuid tomcat
+  setgid tomcat
 
-         setuid tomcat
-         setgid tomcat
+  env JAVA_HOME=/usr/lib/jvm/java-8-oracle
+  env CATALINA_HOME=/opt/tomcat
 
-         env JAVA_HOME=/usr/java/jdk1.8.0_60/jre
-         env CATALINA_HOME=/opt/tomcat
+  # Modify these options as needed
+  env JAVA_OPTS="-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom"
+  env CATALINA_OPTS="-Xms512M -Xmx1024M -server -XX:+UseParallelGC"
 
-         # Modify these options as needed
-         env JAVA_OPTS="-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom"
-         env CATALINA_OPTS="-Xms512M -Xmx1024M -server -XX:+UseParallelGC"
-
-         exec $CATALINA_HOME/bin/catalina.sh run
-
-        # cleanup temp directory after stop
-        post-stop script
-        rm -rf $CATALINA_HOME/temp/*
-        end script
+  exec $CATALINA_HOME/bin/catalina.sh run
+  # cleanup temp directory after stop
+  post-stop script
+    rm -rf $CATALINA_HOME/temp/*
+  end script
 TOMCATCONFIG
 
-	initctl reload-configuration
-	initctl start tomcat
+initctl reload-configuration
+initctl start tomcat
 
 	rm ~/apache-tomcat-8.0.35.tar.gz
 	
+	cd ~
+
 	#Install Priam 3.x
 	git clone https://github.com/Netflix/Priam.git
-	cd Priam
+	cd ~/Priam
 	git checkout 3.x
 	
 	./gradlew build
@@ -129,9 +138,18 @@ TOMCATCONFIG
 	
 	#add the priam javaagent
 
+cat <<CRED > /etc/awscredential.properties
+#This will be replaced by IAM roles
+AWSACCESSID=YOUR_AWS_ACCESS_ID
+AWSKEY=YOUR_AWS_SECRET
+CRED
+
 cat <<JAVAAGENT >> /etc/cassandra/cassandra-env.sh 
-	JVM_OPTS="$JVM_OPTS -javaagent:$CASSANDRA_HOME/lib/priam-cass-extensions-3.2.0-SNAPSHOT.jar"
+JVM_OPTS="$JVM_OPTS -javaagent:$CASSANDRA_HOME/lib/priam-cass-extensions-3.2.0-SNAPSHOT.jar"
 JAVAAGENT
+
+	#Do this to save yourself a few days worth of debugging
+	cp /opt/tomcat/webapps/Priam/WEB-INF/lib/snappy-java-1.0.5.jar /opt/tomcat/lib/
 
 elif [ "${INSTALLFOR}" = "RedHat" -o "${INSTALLFOR}" = "LinuxAMI" ] ; then
 
